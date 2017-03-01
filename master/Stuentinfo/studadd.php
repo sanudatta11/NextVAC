b1db105571dcaa0a7155cedc40eba2f20178e193
sanudatta11 <sanudatta11@gmail.com>
2017-03-01 14:50:05 +0530

    nothing new

--- master/Stuentinfo/studadd.php
+++ master/Stuentinfo/studadd.php
@@ -6,8 +6,39 @@
  * Time: 2:20 PM
  */
 
+/*
+ * Making few notes for future Soumyajit aka Stuxnet
+ * I am making the Profile pic of every student to be default to a picture and then change it according to you
+ * The attendance will be 0 for every student t start and the rank will be the maximum of the field that has already be mentioned
+ * Make the status as I am new to NextVAC
+ * Same for cover make it a default one
+ * */
+
+//    Include Database Connetors
+require_once $_SERVER['DOCUMENT_ROOT'].'/confidential/connector.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/confidential/mysql_login.php';
+
+
 if(isset($_SESSION['designation']) && isset($_SESSION['backauth']) && $_SESSION['designation'] == 'master' && $_SESSION['backauth'] = true)
 {
+    //Checking Both Login and Studentinfo DB table data for saving
+    if(isset($_POST['username']) && isset($_POST['section']) && isset($_POST['regno']))
+    {
+//        Make the attendance to be just 0 and make the rank to be the mazimum plus 1 to the original value
+//        Checking the Profile Table data has came or not
+        if(isset($_POST['email']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['hometown']) && isset($_POST['number']) && isset($_POST['course']) && isset($_POST['senester']) )
+        {
+            
+        }
+        else{
+
+        }
+
+    }
+    else{
+
+    }
+
 
 }
 else if(isset($_SESSION['designation']) && $_SESSION['designation'] == 'master')
