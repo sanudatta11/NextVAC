8c04dc488a49b5c6eeb443b98441c0d9afc19541
sanudatta11 <sanudatta11@gmail.com>
2017-03-01 16:23:47 +0530

    Removed Jquery Tatti

--- js/codingground/codesub.js
+++ js/codingground/codesub.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     //Copying the editors text to desired textarea so that I can post it using form
 
     $('#runbt').hover(function() {
-       // console.log("Hi  run");
+        // console.log("Hi  run");
         var textarea_obj=document.getElementById('code_arena_run');
         /* Stuff to do when the mouse enters the element */
         var val1=editor.getValue();
@@ -42,46 +42,30 @@ $(document).ready(function() {
     });
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
     // Taking event for runfom
 
     $('#runform').submit(function(){
         $('#sampleout').hide();
-<<<<<<< HEAD
-        $('#runbt').prop('disabled',true);
-        $('#runbt').addClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
         $('#processingbox').empty();
 
         $('#processingbox').append('<div class="content" id="processdiv"><br>'+
-        '<p class="text-center">'+
+            '<p class="text-center">'+
             '<h2 class="text text-center" style="color: lightseagreen">'+
             '<strong>Running your code....</strong>'+
-        '</h2>'+
-        '</p>'+
-        '<p class="text-center">'+
+            '</h2>'+
+            '</p>'+
+            '<p class="text-center">'+
             '<embed src="../../images/spinner.gif" style="height: 300px; width: 350px; ">'+
             '</p>'+
-<<<<<<< HEAD
-        '</div>');
-=======
             '</div>');
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
 
         $('#processingbox').show("blind", {times: 10,distance: 100}, 1000);
         $('html,body').animate({
                 scrollTop: $("#processingbox").offset().top},
             'slow');
 
-<<<<<<< HEAD
-=======
         $("#runbt").attr("disabled", true);
         $('#runbt').addClass('disabled');
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
         //console.log("Sub");
         // Maybe show a loading indicator...
         $.post($(this).attr('action'), $(this).serialize(), function(res){
@@ -99,10 +83,6 @@ $(document).ready(function() {
             console.log(obj);
             $('#sampleout').show();
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
             if(obj.errors == true)
             {
                 //Sincere errors as of now
@@ -110,10 +90,10 @@ $(document).ready(function() {
             }
             else if(obj.verdict == 0)
             {
-               $('#ginput').html(obj.dinput);
-               $('#goutput').html(obj.doutput);
+                $('#ginput').html(obj.dinput);
+                $('#goutput').html(obj.doutput);
                 $('#youtput').html(obj.output);
-               $('#sampleverdict').html('Compilation Error');
+                $('#sampleverdict').html('Compilation Error');
             }else if(obj.verdict == 1)
             {
                 var input_desired =obj.dinput;
@@ -150,10 +130,6 @@ $(document).ready(function() {
                 $('#youtput').html('No Output generated');
                 $('#sampleverdict').html('Time Limit Exceeded!');
 
-<<<<<<< HEAD
-                $('#runbt').removeClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
             }else if(obj.verdict == 2)
             {
                 var input_desired =obj.dinput;
@@ -205,10 +181,6 @@ $(document).ready(function() {
                 }
                 $('#youtput').html(html_out);
 
-<<<<<<< HEAD
-                $('#runbt').removeClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
             }else if(obj.verdict == 3)
             {
 
@@ -262,23 +234,14 @@ $(document).ready(function() {
                 }
                 $('#youtput').html(html_out);
 
-<<<<<<< HEAD
-                $('#runbt').removeClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
             }else if(obj.verdict == 4)
             {
                 //Unknown Error
             }
 
         });
-<<<<<<< HEAD
-        $('#runbt').prop('disabled',false);
-        // $('#runbt').removeClass('disabled');
-=======
         $('#runbt').removeClass('disabled');
         $('#runbt').removeAttr("disabled");
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
         return false; // prevent default action
 
     });
@@ -290,10 +253,6 @@ $(document).ready(function() {
 
 
     $('#subform').submit(function(){
-<<<<<<< HEAD
-        $('#subbt').addClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
         $('#processingbox').empty();
         $('#sampleout').hide();
         //console.log("Sub");
@@ -327,7 +286,7 @@ $(document).ready(function() {
             var tot_test = obj.number;
 
             // Success:
-                // <div class="col-sm-4 " style="color: green; " id = "border "><br><br><a href="# "><span class="glyphicon glyphicon-ok " style="color: green; " ></span></a><b> TEST CASE 1 </b><br><br></div>
+            // <div class="col-sm-4 " style="color: green; " id = "border "><br><br><a href="# "><span class="glyphicon glyphicon-ok " style="color: green; " ></span></a><b> TEST CASE 1 </b><br><br></div>
 
             // Failure
             // <div class="col-sm-4 " id="border " style="color: red; "><br><br><a href="# "><span class="glyphicon glyphicon-remove" style="color: red; " ></span></a> <b> TEST CASE 6 </b> <br><br></div>
@@ -336,14 +295,14 @@ $(document).ready(function() {
             //<div class="col-sm-4 " id="border " style="color: #eeaa00; "><br><br><a href="# "><span class=" glyphicon glyphicon-hourglass"  style="color: #eeaa00; "></span></a> <b> TEST CASE 4 </b> <br><br></div>
 
             $('#processingbox').append('<div class="container">' +
-                    '<div class="content" style="box-shadow: 10px 10px 10px 10px #888888; border-radius: 20px;">' +
-                        '<div class="well well-sm " style="border-radius: 20px;">' +
-                            '<h2 class="text text-center" style="color: lightseagreen" id="verdict_text">' +
-                        '   <div id="main_verdict"><strong> VERDICT</strong></div> </h2>' +
-                        '</div>' +
-                        '<div class="row" style="padding-left: 30px;" id="result">' +
-                        '</div>'+
-                    '</div>' +
+                '<div class="content" style="box-shadow: 10px 10px 10px 10px #888888; border-radius: 20px;">' +
+                '<div class="well well-sm " style="border-radius: 20px;">' +
+                '<h2 class="text text-center" style="color: lightseagreen" id="verdict_text">' +
+                '   <div id="main_verdict"><strong> VERDICT</strong></div> </h2>' +
+                '</div>' +
+                '<div class="row" style="padding-left: 30px;" id="result">' +
+                '</div>'+
+                '</div>' +
                 '</div>');
             if(obj.errortop == false)
             {
@@ -377,10 +336,6 @@ $(document).ready(function() {
                     $('#verdict_text').css('color','limegreen');
                     $('#main_verdict').empty();
                     $('#main_verdict').append('<strong>Congratulations!</strong>');
-<<<<<<< HEAD
-                    $('#subbt').removeClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
                 }
                 else if($flag_status == 'wrong')
                 {
@@ -388,20 +343,12 @@ $(document).ready(function() {
                     $('#verdict_text').css('color','red');
                     $('#main_verdict').empty();
                     $('#main_verdict').append('<strong>Wrong Answer</strong>');
-<<<<<<< HEAD
-                    $('#subbt').removeClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
                 }
                 else if($flag_status == 'tle')
                 {
                     $('#verdict_text').css('color','#eeaa00');
                     $('#main_verdict').empty();
                     $('#main_verdict').append('<strong>Time Limit Exceeded!</strong>');
-<<<<<<< HEAD
-                    $('#subbt').removeClass('disabled');
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
                 }
             }
             else if(obj.errortop =='compileerror')
@@ -411,42 +358,22 @@ $(document).ready(function() {
                 $('#main_verdict').empty();
                 $('#main_verdict').append('<strong>Compilation Error</strong>');
                 $('#result').empty();
-<<<<<<< HEAD
-                $('#result').append('<pre  style="max-width: 1100px; align-self: center;"><wre>'+obj.compileerror+'</wre></pre>');
-                $('#subbt').removeClass('disabled');
-=======
                 $('#result').append('<pre>'+obj.compileerror+'</pre>');
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
             }
 
 
         });
-<<<<<<< HEAD
-
-=======
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
         return false; // prevent default action
 
     });
 
     //Changing the language and also the theme below with the help with Jquery
     $("#language").change(function(e) {
-<<<<<<< HEAD
-
-        editor.getSession().setMode("ace/mode/" + this.value);
-        var lang_name = $("#language option:selected").text();
-
-        $('.language').val(lang_name);
-
-    });
-
-=======
         //console.log(this.value);
         editor.getSession().setMode("ace/mode/" + this.value);
         var lang_name = $("#language option:selected").text();
         $('.language').val(lang_name);
     });
->>>>>>> ac781e3feb74f6d6e5697f3230547ba5cd2cc5f1
     $("#theme").change(function(e) {
         console.log(this.value);
         editor.setTheme("ace/mode/"+this.value);
--- master/Stuentinfo/studadd.php
+++ master/Stuentinfo/studadd.php
@@ -9,7 +9,7 @@
 /*
  * Making few notes for future Soumyajit aka Stuxnet
  * I am making the Profile pic of every student to be default to a picture and then change it according to you
- * The attendance will be 0 for every student t start and the rank will be the maximum of the field that has already be mentioned
+ * The attendance will be 0 for every student t start and the rank will be 0
  * Make the status as I am new to NextVAC
  * Same for cover make it a default one
  * */
@@ -17,6 +17,9 @@
 //    Include Database Connetors
 require_once $_SERVER['DOCUMENT_ROOT'].'/confidential/connector.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/confidential/mysql_login.php';
+
+//Including the Random Generator
+require_once $_SERVER['DOCUMENT_ROOT'].'/dependencies/randomize/randomstring.php';
 
 
 if(isset($_SESSION['designation']) && isset($_SESSION['backauth']) && $_SESSION['designation'] == 'master' && $_SESSION['backauth'] = true)
@@ -26,9 +29,89 @@ if(isset($_SESSION['designation']) && isset($_SESSION['backauth']) && $_SESSION[
     {
 //        Make the attendance to be just 0 and make the rank to be the mazimum plus 1 to the original value
 //        Checking the Profile Table data has came or not
-        if(isset($_POST['email']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['hometown']) && isset($_POST['number']) && isset($_POST['course']) && isset($_POST['senester']) )
+        if(isset($_POST['email']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['hometown']) && isset($_POST['number']) && isset($_POST['course']) && isset($_POST['semester']) )
         {
-            
+            /*
+             *List of What I am Saving Actually
+             * Username = Registration No or UID
+             * Section
+             * Regno=Can be Same as Username or maybe Something different
+             * Email=Email of the Student
+             * Firstname and Lastname
+             * Hostel (As of Now a Must) So we will make it stagnant as day Scholar if not mentioned
+             * Hometown and Phone Number
+             * Course and Semester too
+             * Gender too  Cannot be MIDDLE offcourse :-p
+             * */
+
+//            Making the Data Pure as Virgin
+
+            $username = preg_replace("/[^0-9]+/","",$_POST['username']);
+            $password = generaterandomstring(10);
+            $section = preg_replace("/[^A-Za-z0-9]+/","",$_POST['section']);
+            $regno = preg_replace("/[^0-9]+/","",$_POST['regno']);
+            $email = preg_replace("/[^A-Za-z0-9]+/","",$_POST['email']);
+            $firstname = preg_replace("/[^A-Za-z0-9]+/","",$_POST['firstname']);
+            $lastname = preg_replace("/[^A-Za-z0-9]+/","",$_POST['lastname']);
+            $hometown = preg_replace("/[^A-Za-z0-9]+/","",$_POST['hometown']);
+            $phone = preg_replace("/[^A-Za-z0-9]+/","",$_POST['number']);
+            $course = preg_replace("/[^A-Za-z0-9]+/","",$_POST['course']);
+            $semester = preg_replace("/[^A-Za-z0-9]+/","",$_POST['semester']);
+            $name = $firstname.' '.$lastname;
+
+            if(isset($_POST['hostel']))
+            {
+                $hostel = preg_replace("/[^A-Za-z0-9-]+/","",$_POST['hostel']);
+            }
+            else
+            {
+                $hostel = 'Day Scholar';
+            }
+
+            //Generating Random SecretKey For the user. Using
+            $secretkey = sha1((string)$username.generaterandomstring(5));
+            $sessionvar = sha1('invalid');
+
+            //Saving the Login DB value
+            $conn_obj_add= $mysql_conn->prepare('INSERT INTO nextvac.login (username,password,secretkey,sessionvar,designation) VALUES (:username,:pass,:seckey,:sesvar,:desig)');
+            $conn_obj_add->bindParam(':username',$username,PDO::PARAM_INT);
+            $conn_obj_add->bindParam(':pass',$password,PDO::PARAM_STR);
+            $conn_obj_add->bindParam(':seckey',$secretkey,PDO::PARAM_STR);
+            $conn_obj_add->bindParam(':sesvar',$sessionvar,PDO::PARAM_STR);
+            $conn_obj_add->bindValue(':desig','student');
+
+            $conn_obj_add->execute();
+
+            //Saving it in the studentinfo DB
+            $conn_obj_add= $mysql_conn->prepare('INSERT INTO nextvac.studentinfo (secretkey,name,section,regno,attendance,rank) VALUES (:seckey,:name,:section,:regno,:attendance,:rank)');
+            $conn_obj_add->bindParam(':seckey',$secretkey,PDO::PARAM_STR);
+            $conn_obj_add->bindParam(':name',$name, PDO::PARAM_STR);
+            $conn_obj_add->bindParam(':section',$section, PDO::PARAM_STR);
+            $conn_obj_add->bindParam(':regno',$regno, PDO::PARAM_STR);
+            $conn_obj_add->bindValue(':attendance',0,PDO::PARAM_INT);
+            $conn_obj_add->bindValue(':rank',0,PDO::PARAM_INT);
+
+            $conn_obj_add->execute();
+
+            //Saving in Profile DB
+            $conn_obj_add= $mysql_conn->prepare('INSERT INTO nextvac.profile (secretkey,propic,status,designation,incomming,messages,email,firstname,lastname,cover,hostel,hometown,number,course,semester,gender) VALUES (:seckey,:propic,:status,:designation,:incomming,:messages,:email,:first,:last,:cover,:hostel,:hometown,:num,:course,:sem,gender)');
+            $conn_obj_add->bindParam(':seckey',$secretkey,PDO::PARAM_STR);
+
+            $_POST['gender'] = preg_replace("/[^0-9]+/", "",$_POST['gender']);
+
+            if($_POST['gender'] == 1 || $_POST['gender'] == 2)
+            {
+                $gender = $_POST['gender'];
+            }else
+            {
+                $gender = 3;
+            }
+
+            $conn_obj_add->execute();
+
+
+
+
         }
         else{
 
